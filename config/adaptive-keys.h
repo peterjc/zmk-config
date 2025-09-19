cef8ac76d8a4c765a6b0209788c4b4a16fa1f869
Peter J. A. Cock <p.j.a.cock@googlemail.com>
2025-09-19 10:11:11 +0100

    Adaptive keys: qa -> qua, qe -> que, qi -> qui, qo -> quo

--- config/adaptive-keys.h
+++ config/adaptive-keys.h
@@ -18,6 +18,8 @@
             bindings = <&kp A>;
 
             akA_Cap { trigger-keys = <CMMA>; max-prior-idle-ms = <my_cc_term>; bindings = <&kp BSPC &kp LS(A)>; strict-modifiers; };
+            // qa -> qua:
+            akQA_t_ua { trigger-keys = <Q>; max-prior-idle-ms = <my_ak_term>; bindings = <&kp U &kp A>; strict-modifiers; };
         };
         ak_B: ak_B {
             compatible = "zmk,behavior-adaptive-key";
@@ -46,6 +48,8 @@
             bindings = <&kp E>;
 
             akE_Cap { trigger-keys = <CMMA>; max-prior-idle-ms = <my_cc_term>; bindings = <&kp BSPC &kp LS(E)>; strict-modifiers; };
+            // qe -> que
+            akQE_t_ue { trigger-keys = <Q>; max-prior-idle-ms = <my_ak_term>; bindings = <&kp U &kp E>; strict-modifiers; };
         };
         ak_F: ak_F {
             compatible = "zmk,behavior-adaptive-key";
@@ -75,6 +79,8 @@
             bindings = <&kp I>;
 
             akI_Cap { trigger-keys = <CMMA>; max-prior-idle-ms = <my_cc_term>; bindings = <&kp BSPC &kp LS(I)>; strict-modifiers; };
+            // qi -> qui
+            akQI_t_ui { trigger-keys = <Q>; max-prior-idle-ms = <my_ak_term>; bindings = <&kp U &kp I>; strict-modifiers; };
         };
         ak_J: ak_J {
             compatible = "zmk,behavior-adaptive-key";
@@ -118,6 +124,8 @@
             bindings = <&kp O>;
 
             akO_Cap { trigger-keys = <CMMA>; max-prior-idle-ms = <my_cc_term>; bindings = <&kp BSPC &kp LS(O)>; strict-modifiers; };
+            // qo -> quo
+            akQO_t_uo { trigger-keys = <Q>; max-prior-idle-ms = <my_ak_term>; bindings = <&kp U &kp O>; strict-modifiers; };
         };
         ak_P: ak_P {
             compatible = "zmk,behavior-adaptive-key";
